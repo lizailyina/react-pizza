49e6971268c8b439940d10661e06fdc2b5b41ea2
lizailyina <liza-ilina-13052005@mail.ru>
2023-02-28 20:47:27 -0600

    add categories logic

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+const categories = ["All", "Meat", "Vegan", "Grill", "Hot", "Closed"]
+
 export const Categories = () => {
+
+  const [active, setActive] = React.useState(categories[0]);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item) => (
+          <li
+            key={item}
+            className={item === active ? "active" : ""}
+            onClick={() => setActive(item)}>{item}</li>
+        ))}
       </ul>
     </div>
   )
